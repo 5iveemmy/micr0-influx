e3f1a3f6075d7e35b9a7f340881afe41056fb130
5iveemmy <ainemmanuel87@gmail.com>
2024-10-05 00:24:17 +0100

    project init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          Hello Vite + React + Tailwind + Ant Design!
+        </h1>
+        <Button type="primary">Ant Design Button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
