05fb8cc6a6f27349b387eae69f002524c0df526f
5iveemmy <ainemmanuel87@gmail.com>
2024-10-05 23:16:31 +0100

    chore: add antd config provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { ConfigProvider } from "antd";
 import { AppRoutes } from "./routes";
 
+const customTheme = {
+  token: {
+    colorText: "#0D1821",
+  },
+};
+
 function App() {
-  return <AppRoutes />;
+  return (
+    <ConfigProvider theme={customTheme}>
+      <AppRoutes />
+    </ConfigProvider>
+  );
 }
 
 export default App;
